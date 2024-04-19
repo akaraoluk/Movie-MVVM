34c88c120fa18cc8b01c9778e93db1693b8de6af
akaraoluk <abdurrahmankaraoluk@gmail.com>
2024-04-19 22:42:07 +0300

    [Movie-MVVM][Version 1][Abdurrahman KARAOLUK][-]

---
+++ Movie-MVVM/AppDelegate.swift
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.swift
+//  Movie-MVVM
+//
+//  Created by Abdurrahman Karaoluk on 19.04.2024.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        let navigationController = UINavigationController(rootViewController: MainViewController())
+        window.rootViewController = navigationController
+        window.makeKeyAndVisible()
+        self.window = window
+        
+        return true
+    }
+
+
+}
+
---
+++ Movie-MVVM/Controllers/MainViewController/MainViewController.swift
@@ -0,0 +1,31 @@
+//
+//  MainViewController.swift
+//  Movie-MVVM
+//
+//  Created by Abdurrahman Karaoluk on 19.04.2024.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        self.title = "Main View"
+        self.view.backgroundColor = .red
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
