7eecf05acdae132109e099e381cc793990210fff
akaraoluk <abdurrahmankaraoluk@gmail.com>
2024-04-20 11:30:17 +0300

    [Movie-MVVM][Version 1][Abdurrahman KARAOLUK][tableView Added]

--- Movie-MVVM/Controllers/MainViewController/MainViewController.swift
+++ Movie-MVVM/Controllers/MainViewController/MainViewController.swift
@@ -8,24 +8,25 @@
 import UIKit
 
 class MainViewController: UIViewController {
-
+    
+    //IBOutletes:
+    @IBOutlet var tableView: UITableView!
+    
+    //ViewModel:
+    var viewModel: MainViewModel = MainViewModel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        configureView()
+    }
+
+
+    func configureView() {
         self.title = "Main View"
         self.view.backgroundColor = .red
+        
+        setupTableView()
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
---
+++ Movie-MVVM/ViewModel/MainViewModel.swift
@@ -0,0 +1,19 @@
+//
+//  MainViewModel.swift
+//  Movie-MVVM
+//
+//  Created by Abdurrahman Karaoluk on 20.04.2024.
+//
+
+import Foundation
+
+class MainViewModel {
+    
+    func numberOfSections() -> Int {
+        1
+    }
+    
+    func numberOfRows(in section: Int) -> Int {
+        10
+    }
+}
